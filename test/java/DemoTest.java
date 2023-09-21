b6e5ec2918dca5e733808a17b729629721c19347
Nadine Scherer <armine021@gmail.com>
2023-09-20 17:12:21 -0700

    initial commit

---
+++ test/java/DemoTest.java
@@ -0,0 +1,18 @@
+import java.beans.Transient;
+import static org.junit.Assert.*;
+
+/**
+ * The class containing your tests for the {@link Demo} class.  Make sure you
+ * test all methods in this class (including both 
+ * {@link Demo#main(String[])} and 
+ * {@link Demo#isTriangle(double, double, double)}).
+ */
+public class DemoTest {
+
+    @Test
+    public void test_is_triangle_1() {
+        assertTrue(Demo.isTriangle(3,4,5));
+    }
+
+
+}
