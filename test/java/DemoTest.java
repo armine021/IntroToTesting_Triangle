5c6e2129e86d35f40c433fc369d17025a6eabf0a
Nadine Scherer <armine021@gmail.com>
2023-09-27 17:03:08 -0700

    float tests

--- test/java/DemoTest.java
+++ test/java/DemoTest.java
@@ -9,27 +9,27 @@ import static org.junit.Assert.*;
  */
 public class DemoTest {
 
-    @Test
-    public void test_is_triangle_1() {
-        assertTrue(Demo.isTriangle(3,4,5));
-    }
+  @Test
+  public void test_is_triangle_1() {
+    assertTrue(Demo.isTriangle(3,4,5));
+  }
 
-    @Test
-    public void test_is_triangle_2() {
-        assertTrue(Demo.isTriangle(4,3,5));
-    }
+  @Test
+  public void test_is_triangle_2() {
+    assertTrue(Demo.isTriangle(4,3,5));
+  }
 
 	@Test
 	public void TestIsNOTTriangle1() {
 		assertFalse(Demo.isTriangle(30, 10, 40));
    }
 
-   @Test
+  @Test
 	public void TestIsNOTTriangle2() {
 		assertFalse(Demo.isTriangle(10, 30, 40));
    }
 
-      @Test
+  @Test
 	public void TestIsNOTTriangle3() {
 		assertFalse(Demo.isTriangle(10, 40, 30));
    }
@@ -39,19 +39,33 @@ public class DemoTest {
 		assertFalse(Demo.isTriangle(-3, 10, 20));
     }
 
-    	@Test
+  @Test
 	public void TestIsNOTTriangle5() {
 		assertFalse(Demo.isTriangle(10, -3, 20));
     }
 
-    	@Test
+  @Test
 	public void TestIsNOTTriangle6() {
 		assertFalse(Demo.isTriangle(20, 10, -3));
     }
 
-    @Test
+  @Test
 	public void TestIsNOTTriangle7() {
 		assertFalse(Demo.isTriangle(0, 10, 20));
     }
 
+  @Test
+	public void TestIsNOTTriangle8() {
+		assertFalse(Demo.isTriangle(1.4, 10, 20));
+    }
+
+  @Test
+	public void TestIsNOTTriangle9() {
+		assertFalse(Demo.isTriangle(1.4, 10.3, 20));
+    }
+
+  @Test
+	public void TestIsNOTTriangle10() {
+		assertFalse(Demo.isTriangle(1.4, 10, 20.7));
+    }
 }
